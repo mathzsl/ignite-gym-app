7c7c33334f5b5bd46a053bc1a3fa0c4ee52559ec
mathzsl <maths.soares99@gmail.com>
2023-11-26 20:33:29 -0300

    chore: initial project setup

---
+++ mobile/App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { NativeBaseProvider } from 'native-base'
+
+import { Loading } from '@components/Loading'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  return (
+    <NativeBaseProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <View /> : <Loading />}
+    </NativeBaseProvider>
+  )
+}
