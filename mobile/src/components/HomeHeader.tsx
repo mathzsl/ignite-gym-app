3ba1b71d7824f188306667d20437d8f5ea681e4e
mathzsl <maths.soares99@gmail.com>
2023-11-28 02:10:55 -0300

    feat: HomeHeader component

---
+++ mobile/src/components/HomeHeader.tsx
@@ -0,0 +1,31 @@
+import { TouchableOpacity } from 'react-native'
+import { HStack, Heading, Text, VStack, Icon } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { Avatar } from './Avatar'
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <Avatar
+        source={{ uri: 'https://www.github.com/mathzsl.png' }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+
+      <VStack mr={4} flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md" numberOfLines={1}>
+          Matheus
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
