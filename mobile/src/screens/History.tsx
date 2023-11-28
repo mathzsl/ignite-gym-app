27a85156a4f32ccb4c4f5e1e85926ffb9a2443dc
mathzsl <maths.soares99@gmail.com>
2023-11-28 04:02:28 -0300

    feat: visual structure of History screen

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -1,9 +1,50 @@
-import { Center, Text } from 'native-base'
+import { useState } from 'react'
+import { Heading, VStack, SectionList, Text } from 'native-base'
+
+import { HistoryCard } from '@components/HistoryCard'
+import { ScreenHeader } from '@components/ScreenHeader'
 
 export function History() {
+  const [exerciseHistory, setExerciseHistory] = useState([
+    {
+      title: '09/10/2023',
+      data: ['1', '2', '3'],
+    },
+    {
+      title: '10/10/2023',
+      data: ['1', '2'],
+    },
+    {
+      title: '11/10/2023',
+      data: ['1', '2', '3', '4'],
+    },
+  ])
+
   return (
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        showsVerticalScrollIndicator={false}
+        sections={exerciseHistory}
+        keyExtractor={(item) => item}
+        renderItem={() => <HistoryCard />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={
+          exerciseHistory.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.200" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'} Vamos fazer exercícios
+            hoje?
+          </Text>
+        )}
+      />
+    </VStack>
   )
 }
