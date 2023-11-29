776eaf00d3848cda114f6cf155309d95f5be3bb8
mathzsl <maths.soares99@gmail.com>
2023-11-28 21:11:21 -0300

    feat: adding action of selecting an image

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { TouchableOpacity } from 'react-native'
+
 import {
   Center,
-  Heading,
   ScrollView,
   Skeleton,
   Text,
   VStack,
+  useToast,
 } from 'native-base'
 
 import { Input } from '@components/Input'
@@ -14,10 +15,55 @@ import { Button } from '@components/Button'
 import { Avatar } from '@components/Avatar'
 import { ScreenHeader } from '@components/ScreenHeader'
 
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
+import { FileInfo } from 'expo-file-system'
+
+import userPhotoDefault from '@assets/userPhotoDefault.png'
+
 const PHOTO_SIZE = 33
 
 export function Profile() {
-  const [photoIsLoading, setPhotoIsLoading] = useState(true)
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [selectedImage, setSelectedImage] = useState<null | string>(null)
+
+  const toast = useToast()
+
+  async function handleSelectPickImage() {
+    setPhotoIsLoading(true)
+
+    try {
+      const imagePickerResult = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      })
+
+      if (imagePickerResult.canceled) {
+        return null
+      }
+
+      if (!imagePickerResult.canceled) {
+        const photoInfo = (await FileSystem.getInfoAsync(
+          imagePickerResult.assets[0].uri,
+        )) as FileInfo
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 1) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB',
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+        }
+
+        setSelectedImage(imagePickerResult.assets[0].uri)
+      }
+    } catch (error) {
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -31,16 +77,19 @@ export function Profile() {
             rounded="full"
             startColor="gray.500"
             endColor="gray.400"
-            isLoaded={photoIsLoading}
+            isLoaded={!photoIsLoading}
           >
             <Avatar
-              source={{ uri: 'https://www.github.com/mathzsl.png' }}
+              source={selectedImage ? { uri: selectedImage } : userPhotoDefault}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           </Skeleton>
 
-          <TouchableOpacity style={{ marginTop: 12, marginBottom: 32 }}>
+          <TouchableOpacity
+            style={{ marginTop: 12, marginBottom: 32 }}
+            onPress={handleSelectPickImage}
+          >
             <Text fontWeight="bold" fontSize="md" color="green.500">
               Alterar foto
             </Text>
