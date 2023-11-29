576b9ef87b65e0ee435d924387dcaa31319148fc
mathzsl <maths.soares99@gmail.com>
2023-11-29 19:34:33 -0300

    fix: added error message

--- mobile/src/components/Input.tsx
+++ mobile/src/components/Input.tsx
@@ -1,23 +1,37 @@
-import { Input as NativeInput, IInputProps } from 'native-base'
+import { Input as NativeInput, IInputProps, FormControl } from 'native-base'
 
-export function Input({ ...rest }: IInputProps) {
+type InputProps = IInputProps & {
+  errorMessage?: string | null
+}
+
+export function Input({ errorMessage = null, isInvalid, ...rest }: InputProps) {
+  const invalid = !!errorMessage || isInvalid
+
   return (
-    <NativeInput
-      bg="gray.700"
-      h={14}
-      px={4}
-      borderWidth={0}
-      fontSize="md"
-      color="white"
-      fontFamily="body"
-      placeholderTextColor="gray.300"
-      mb={4}
-      _focus={{
-        bg: 'gray.700',
-        borderWidth: 1,
-        borderColor: 'green.500',
-      }}
-      {...rest}
-    />
+    <FormControl isInvalid={invalid} mb={4}>
+      <NativeInput
+        bg="gray.700"
+        h={14}
+        px={4}
+        borderWidth={0}
+        fontSize="md"
+        color="white"
+        fontFamily="body"
+        placeholderTextColor="gray.300"
+        isInvalid={invalid}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: 'red.500',
+        }}
+        _focus={{
+          bg: 'gray.700',
+          borderWidth: 1,
+          borderColor: 'green.500',
+        }}
+        {...rest}
+      />
+
+      <FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
+    </FormControl>
   )
 }
