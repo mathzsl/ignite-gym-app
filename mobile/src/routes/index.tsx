1eb38fabf4a59b190618ec18b07b1af43936ff9b
mathzsl <maths.soares99@gmail.com>
2023-12-02 21:40:29 -0300

    fix: displaying loading

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -7,12 +7,18 @@ import { useAuth } from '@hooks/useAuth'
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
+import { Loading } from '@components/Loading'
+
 export function Routes() {
-  const { user } = useAuth()
+  const { user, isLoadingStoredUserData } = useAuth()
   const { colors } = useTheme()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray['700']
+
+  if (isLoadingStoredUserData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg="gray.700">
