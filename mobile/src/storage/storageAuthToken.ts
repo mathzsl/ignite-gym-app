330011e532187c763eda90c593b948471929036b
mathzsl <maths.soares99@gmail.com>
2023-12-03 08:04:31 -0300

    feat: persisting user token

--- mobile/src/storage/storageAuthToken.ts
+++ mobile/src/storage/storageAuthToken.ts
@@ -5,3 +5,13 @@ import { AUTH_TOKEN_STORAGE } from '@storage/storageConfig'
 export async function savedAuthTokenStorage(token: string) {
   await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token)
 }
+
+export async function getStoredAuthToken() {
+  const token = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE)
+
+  return token
+}
+
+export async function removeStoredAuthToken() {
+  AsyncStorage.removeItem(AUTH_TOKEN_STORAGE)
+}
