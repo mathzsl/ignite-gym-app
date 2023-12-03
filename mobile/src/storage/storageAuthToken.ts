4ba1ac302a4c28c0761c251296f977479fa87492
mathzsl <maths.soares99@gmail.com>
2023-12-02 23:33:55 -0300

    feat: savedAuthTokenStorage funcion

---
+++ mobile/src/storage/storageAuthToken.ts
@@ -0,0 +1,7 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { AUTH_TOKEN_STORAGE } from '@storage/storageConfig'
+
+export async function savedAuthTokenStorage(token: string) {
+  await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token)
+}
