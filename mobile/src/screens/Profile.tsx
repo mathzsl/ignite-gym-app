e31d0552aa4d1ef73d897252baceea2b4498a0b5
mathzsl <maths.soares99@gmail.com>
2023-12-03 21:04:06 -0300

    feat: new functionality update profile

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -10,6 +10,10 @@ import {
   useToast,
 } from 'native-base'
 
+import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 import { Avatar } from '@components/Avatar'
@@ -21,13 +25,60 @@ import { FileInfo } from 'expo-file-system'
 
 import userPhotoDefault from '@assets/userPhotoDefault.png'
 
+import { useAuth } from '@hooks/useAuth'
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
+
 const PHOTO_SIZE = 33
 
+const profileFormSchema = yup.object({
+  name: yup.string().required('Informe o nome.'),
+  email: yup.string().email(),
+
+  old_password: yup.string(),
+
+  password: yup
+    .string()
+    .min(6, 'A senha deve ter pelo menos 6 digitos.')
+    .nullable()
+    .transform((value) => value || null),
+
+  confirm_new_password: yup
+    .string()
+    .nullable()
+    .transform((value) => value || null)
+    .oneOf([yup.ref('password')], 'A confirmação de senha não confere.')
+    .when('password', {
+      is: (Field: any) => Field,
+      then: (schema) =>
+        schema
+          .nullable()
+          .required('Informe a confirmação de senha.')
+          .transform((value) => value || null),
+    }),
+})
+
+type ProfileFormData = yup.InferType<typeof profileFormSchema>
+
 export function Profile() {
+  const [isUpdating, setIsUpdating] = useState(false)
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
   const [selectedImage, setSelectedImage] = useState<null | string>(null)
 
   const toast = useToast()
+  const { user, updateUserProfile } = useAuth()
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ProfileFormData>({
+    resolver: yupResolver(profileFormSchema),
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+    },
+  })
 
   async function handleSelectPickImage() {
     setPhotoIsLoading(true)
@@ -65,6 +116,42 @@ export function Profile() {
     }
   }
 
+  async function handleUpdateProfile(data: ProfileFormData) {
+    try {
+      setIsUpdating(true)
+
+      const userUpdated = user
+      userUpdated.name = data.name
+
+      await api.put('/users', {
+        name: data.name,
+        password: data.password,
+        old_password: data.old_password,
+      })
+
+      await updateUserProfile(userUpdated)
+
+      toast.show({
+        title: 'Perfil atualizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500',
+      })
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível atualizar o seu perfil. Tente novamente mais tarde!'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    } finally {
+      setIsUpdating(false)
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -95,9 +182,32 @@ export function Profile() {
             </Text>
           </TouchableOpacity>
 
-          <Input placeholder="Nome" bg="gray.600" />
+          <Controller
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                bg="gray.600"
+                value={value}
+                onChangeText={onChange}
+                errorMessage={errors.name?.message}
+              />
+            )}
+            name="name"
+          />
 
-          <Input bg="gray.600" value="maths.soares99@gmail.com" isDisabled />
+          <Controller
+            control={control}
+            render={({ field: { value } }) => (
+              <Input
+                placeholder="E-mail"
+                bg="gray.600"
+                value={value}
+                isDisabled
+              />
+            )}
+            name="email"
+          />
 
           <Text
             fontSize="md"
@@ -110,17 +220,53 @@ export function Profile() {
             Alterar senha
           </Text>
 
-          <Input placeholder="Senha antiga" bg="gray.600" secureTextEntry />
-
-          <Input placeholder="Nova antiga" bg="gray.600" secureTextEntry />
-
-          <Input
-            placeholder="Confirme a nova senha"
-            bg="gray.600"
-            secureTextEntry
+          <Controller
+            control={control}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Senha antiga"
+                bg="gray.600"
+                secureTextEntry
+                onChangeText={onChange}
+              />
+            )}
+            name="old_password"
           />
 
-          <Button title="Atualizar" mt={5} />
+          <Controller
+            control={control}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Nova senha"
+                bg="gray.600"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+            name="password"
+          />
+
+          <Controller
+            control={control}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Confirme a nova senha"
+                bg="gray.600"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.confirm_new_password?.message}
+              />
+            )}
+            name="confirm_new_password"
+          />
+
+          <Button
+            title="Atualizar"
+            mt={5}
+            isLoading={isUpdating}
+            onPress={handleSubmit(handleUpdateProfile)}
+          />
         </Center>
       </ScrollView>
     </VStack>
